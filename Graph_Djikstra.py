158c672eea50ce78fbdd347839248fa1effa9e0f
lone <tsuyoku2024@gmail.com>
2026-04-13 22:45:15 +0800

    Add Muscle-Memory/Graph_Djikstra.py

---
+++ Graph_Djikstra.py
@@ -0,0 +1,13 @@
+def djikstras(src, n):
+  dist = [float('inf')] * n
+  dist[src] = 0
+  pq = [(0, src)]
+
+  while pq:
+    d, u = heapq.heappop()
+    if d > dist[u]: continue
+    for v, wt in adj[u]:
+      if dist[u] + wt < dist[v]:
+        dist[v] = dist[u] + wt
+        heapq.heappush(pq, (dist[v], v))
+  return dist
