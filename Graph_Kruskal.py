6669006256ea8d2c1df9cfbe1b938a0085d0d8a7
lone <tsuyoku2024@gmail.com>
2026-04-13 22:59:21 +0800

    Add Muscle-Memory/Graph_Kruskal.py

---
+++ Graph_Kruskal.py
@@ -0,0 +1,27 @@
+def kruskal(n, edges):
+  par = list(range(n))
+  sz = [1] * n
+  def find(a):
+    while a != par[a]:
+      par[a] = par[par[a]]
+      a = par[a]
+    return a
+  def union(a, b):
+    a, b = find(a), find(b)
+    if  a == b: return False
+    if sz[a] < sz[b]:
+      a, b = b, a
+    par[b] = a
+    sz[a] += sz[b]
+    return True
+
+  edges.sort()
+  cnt = 0
+  cost = 0
+  # edge
+  for w, u, v in edges:
+    if union(u, v):
+      cost += w
+      cnt += 1
+      if cnt == n-1: break
+  return cost
