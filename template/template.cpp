b9f6cdb3f33f4b8304e1b4ad3ed4956e19acce37
lone <tsuyoku2024@gmail.com>
2026-05-29 18:55:05 +0800

    Create template.cpp

---
+++ template/template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+
+#define pb push_back
+#define eb emplace_back
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)(x).size()
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+void solve() {
+  
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int t; cin >> t;
+  while (t--) {
+    solve();
+  }
+}
