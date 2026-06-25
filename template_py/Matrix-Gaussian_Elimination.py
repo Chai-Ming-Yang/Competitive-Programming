9b3b3cafe5df9575ae2f53f28ae1bc29cd363aef
lone <tsuyoku2024@gmail.com>
2026-06-25 11:06:23 +0800

    Update Matrix-Gaussian_Elimination.py

--- template_py/Matrix-Gaussian_Elimination.py
+++ template_py/Matrix-Gaussian_Elimination.py
@@ -1,23 +1,23 @@
 DIM = int(input())
-matrx = [list(map(int, input().split()))]
+G = [list(map(int, input().split())) for _ in range(DIM)]
 
 # Forward Elimination
-for PIVOT in range(DIM):
-  tmp_pivot = PIVOT
-  for r in range(PIVOT+1, DIM):
-    if abs(matrix[r][PIVOT]) > abs(matrix[tmp_pivot][PIVOT]):
-      tmp_pivot = r
-  matrix[PIVOT], matrix[tmp_pivot] = matrix[tmp_pivot], matrix[PIVOT]
+for PIV in range(DIM):
+  parpiv = PIV
+  for r in range(PIV+1, DIM):
+    if abs(G[r][PIV]) > abs(G[parpiv][PIV]):
+      parpiv = r
+  G[PIV], G[parpiv] = G[parpiv], G[PIV]
 
-  for r in range(PIVOT + 1, DIM):
-    factor = matrix[r][PIVOT] / matrix[PIVOT][PIVOT]
-    for c in range(PIVOT, DIM + 1):
-      matrix[r][c] -= factor * matrix[PIVOT][c]
+  for r in range(PIV+1, DIM):
+    factor = G[r][PIV] / G[PIV][PIV]
+    for c in range(PIV, DIM+1):
+      G[r][c] -= factor * G[PIV][c]
 
 # Backward Propagation
-ans = [0] * DIM
-for PIVOT in range(DIM-1, -1, -1):
-  total = 0
-  for c in range(PIVOT+1, DIM):
-    total += matrix[PIVOT][c] * ans[c]
-  ans[PIVOT] = (matrix[PIVOT][DIM] - total) / matrix[PIVOT][PIVOT]
+res = [0] * DIM
+for PIV in range(DIM-1, -1, -1):
+  res[PIV] = G[PIV][DIM]
+  for c in range(PIV+1, DIM):
+    res[PIV] -= G[PIV][c] * res[c]
+  res[PIV] /= G[PIV][PIV]
